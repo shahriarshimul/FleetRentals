58b780fb3db4678e296bacd981db1e28ad442594
MahiraJabin <mahirajabin0231@gmail.com>
2024-01-09 12:13:50 +0600

    Second Commit

--- resources/views/frontend/components/fixed/header.blade.php
+++ resources/views/frontend/components/fixed/header.blade.php
@@ -1,7 +1,7 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-success">
     <div class="container px-4 px-lg-5 input-group rounded">
-        <a class="navbar-brand text-light" href="{{ route('home') }}" style="font-size: 36px;">FleetFlowRentals</a>
+        <a class="navbar-brand text-light" href="{{ route('home') }}" style="font-size: 36px;">FleetRentals</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
                 class="navbar-toggler-icon"></span></button>
